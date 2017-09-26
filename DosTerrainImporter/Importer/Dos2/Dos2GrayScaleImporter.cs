42a2ddcf3deada9fc9ad1e2956ff5494df577fa8
Kenny De Smedt <de_smedt_kenny@telenet.be>
2017-09-26 21:07:39 +0200

    retrieval of all patch files based on the data file

--- DosTerrainImporter/Importer/Dos2/Dos2GrayScaleImporter.cs
+++ DosTerrainImporter/Importer/Dos2/Dos2GrayScaleImporter.cs
@@ -1,5 +1,6 @@
 ﻿using DosTerrainImporter.Importer.Dos2;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
@@ -10,29 +11,51 @@ namespace DosTerrainImporter.Importer
     class Dos2GrayScaleImporter : TerrainImporter
     {
         private const int MAX_FILE_OPERATIONS_FOR_ONE_PATCH = 1089;
-
+        private const string PATCH_EXTENSION = ".patch";
         PatchFileWriter patchFileWriter = new PatchFileWriter();
         Bitmap bmp = null;
-        String patchFileName = null;
+        String dataFileName = null;
         float minHeight;
         float maxHeight;
         int maxWriteOperations = 0;
+        List<String> patchFiles = new List<String>();
 
-        public Dos2GrayScaleImporter(String patchFileName, String bitmapSourceFile, float minHeight, float maxHeight)
+        public Dos2GrayScaleImporter(String dataFileName, String bitmapSourceFile, float minHeight, float maxHeight)
         {
             this.bmp = new Bitmap(bitmapSourceFile);
             this.maxWriteOperations = getMaximumWriteOperations();
-            this.patchFileName = patchFileName;
+            this.dataFileName = dataFileName;
             this.minHeight = minHeight;
             this.maxHeight = maxHeight;
         }
 
         public override void execute(object sender, DoWorkEventArgs e)
         {
+            CreateListOfPatchFiles();
+            WriteTerrain(sender);
+        }
+
+        private void CreateListOfPatchFiles()
+        {
+            String workingDirectory = Path.GetDirectoryName(dataFileName);
+            String[] files = Directory.GetFiles(workingDirectory);
+            String uuid = Path.GetFileNameWithoutExtension(dataFileName);
+
+            foreach (String file in files)
+            {
+                if(file.Contains(uuid) && file.Contains(PATCH_EXTENSION))
+                {
+                    patchFiles.Add(Path.GetFileNameWithoutExtension(file));
+                }
+            }            
+        }
+
+        private void WriteTerrain(object sender)
+        {
             int bitmap_j_offset = 0;
             int bitmap_i_offset = 0;
 
-            int counter = patchFileWriter.WritePatchFile(patchFileName, bmp, bitmap_j_offset, bitmap_i_offset, minHeight, maxHeight, 1);
+            int counter = patchFileWriter.WritePatchFile(dataFileName, bmp, bitmap_j_offset, bitmap_i_offset, minHeight, maxHeight, 1);
 
             if (counter % 100 == 0 || counter == this.maxWriteOperations)
             {
